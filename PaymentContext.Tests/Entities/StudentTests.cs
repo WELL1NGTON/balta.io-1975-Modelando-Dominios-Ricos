70428b360236785375807d8f02f3cee02dd1c1d8
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-04 09:36:44 -0300

    10..11
    10. Primitive Obsession
    11. Value Objects

--- PaymentContext.Tests/Entities/StudentTests.cs
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -9,9 +9,9 @@ namespace PaymentContext.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            var subscription = new Subscription(null);
-            var student = new Student("André", "Baltieri", "12345678912", "hello@balta.io");
-            student.AddSubscription(subscription);
+            // var subscription = new Subscription(null);
+            // var student = new Student("André", "Baltieri", "12345678912", "hello@balta.io");
+            // student.AddSubscription(subscription);
         }
     }
 }
