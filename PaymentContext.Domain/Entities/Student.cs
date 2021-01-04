6046c01cb43292477c618c7bd577856585dde6fe
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-04 09:50:54 -0300

    12. Implementando Validações

--- PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext.Domain/Entities/Student.cs
@@ -8,13 +8,14 @@ namespace PaymentContext.Domain.Entities
     public class Student : Entity
     {
         private IList<Subscription> _subscriptions;
-        public Student(Name name, Document document, Email email, Address address)
+        public Student(Name name, Document document, Email email)
         {
             Name = name;
             Document = document;
             Email = email;
-            Address = address;
             _subscriptions = new List<Subscription>();
+
+            AddNotifications(name, document, email);
         }
 
         public Name Name { get; private set; }
@@ -25,8 +26,6 @@ namespace PaymentContext.Domain.Entities
 
         public void AddSubscription(Subscription subscription)
         {
-            // Se já tiver uma assinatura ativa, cancela
-
             // Cancela todas as outras assinaturas, e coloca esta como principal
             foreach (var sub in Subscriptions)
             {
