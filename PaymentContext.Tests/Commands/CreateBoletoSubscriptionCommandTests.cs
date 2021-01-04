c09826e299b82cac431ac5e561c307e149b2958d
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-04 13:15:04 -0300

    17. Testando os Commands

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Commands;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class CreateBoletoSubscriptionCommandTests
+    {
+        // Metodologia "Red, Green, Refactor"
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenNamesInvalid()
+        {
+            var command = new CreateBoletoSubscriptionCommand();
+            command.FirstName = "";
+
+            command.Validate();
+            Assert.AreEqual(false, command.Valid);
+        }
+
+    }
+}
